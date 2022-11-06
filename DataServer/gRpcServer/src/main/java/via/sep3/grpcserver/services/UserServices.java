7f0ee1c303a8badc4adfaee8afb5bc70f27e93c4
wael85 <dr.wael85@gmail.com>
2022-11-06 20:07:28 +0100

    add.

--- DataServer/gRpcServer/src/main/java/via/sep3/grpcserver/services/UserServices.java
+++ DataServer/gRpcServer/src/main/java/via/sep3/grpcserver/services/UserServices.java
@@ -1,2 +1,41 @@
-package via.sep3.grpcserver.services;public class UserServices {
+package via.sep3.grpcserver.services;
+
+import io.grpc.stub.StreamObserver;
+import org.lognet.springboot.grpc.GRpcService;
+import org.springframework.beans.factory.annotation.Autowired;
+import via.sep3.grpcserver.Entities.User;
+
+import via.sep3.grpcserver.protobuf.userservices.RequestUserInfo;
+import via.sep3.grpcserver.protobuf.userservices.ResponseUserInfo;
+import via.sep3.grpcserver.protobuf.userservices.UserServicesGrpc;
+import via.sep3.grpcserver.repositorys.UserRepository;
+
+@GRpcService
+public class UserServices extends UserServicesGrpc.UserServicesImplBase {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public void createUser(RequestUserInfo userInfo, StreamObserver<ResponseUserInfo> responseUserInfo) {
+        System.out.println("getCalled");
+        User u = new User();
+        u.setAddress(userInfo.getAddress());
+        u.setDriveLicence(userInfo.getDriveLicense());
+        u.setEmail(userInfo.getEmail());
+        u.setFirstName(userInfo.getFirstName());
+        u.setLastName(userInfo.getLastName());
+        u.setEmail(userInfo.getEmail());
+        u.setPhone(userInfo.getPhone());
+        User resultUser = userRepository.save(u);
+        ResponseUserInfo result = ResponseUserInfo.newBuilder()
+                .setAddress(resultUser.getAddress())
+                .setEmail(resultUser.getEmail())
+                .setFirstName(resultUser.getFirstName())
+                .setLastName(resultUser.getLastName())
+                .setPhone(resultUser.getPhone())
+                .build();
+        responseUserInfo.onNext(result);
+        responseUserInfo.onCompleted();
+
+    }
 }
