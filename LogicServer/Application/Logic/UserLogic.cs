11b31c8cbece32452dc3da784258e29986cef3fc
Semo0 <317546@via.dk>
2022-11-07 12:31:04 +0100

    Create logic and controller to create a user

--- LogicServer/Application/Logic/UserLogic.cs
+++ LogicServer/Application/Logic/UserLogic.cs
@@ -1,9 +1,63 @@
+using System.Text.RegularExpressions;
 using Application.LogicInterfaces;
+using Domain.DTOs;
+using Domain.Model;
 using gRPCClient.GrpcInterfaces;
 
 namespace Application.Logic;
 
 public class UserLogic:IUserLogic
 {
-    private readonly IUserService _service;
+    private readonly IUserService service;
+
+    public UserLogic(IUserService service)
+    {
+        this.service = service;
+    }
+
+    public async Task<ReturnedUserDTO> CreateAsync(User user)
+    {
+        ValidateUSer(user);
+        
+        ReturnedUserDTO toReturn = await service.CreateAsync(user);
+        
+        
+        return toReturn;  
+    }
+
+    private static void ValidateUSer(User user)
+    {
+        if (user.Email.Length<3)
+        {
+            throw new Exception("Email cannot bee less than 3 characters");
+        }
+
+        if (!user.Email.Contains("@"))
+        {
+            throw new Exception("Please enter a valid email missing (@)");
+
+        }
+
+        if (user.Email.Contains("."))
+        {
+            throw new Exception("Email must have a domain ");
+        }
+
+        if (user.Phone.Length<8)
+        {
+            throw new Exception("Phone Number must be at least 8 numbers");
+
+        }
+        if (user.Phone.Length>12)
+        {
+            throw new Exception("Phone Number cannot be more than 12 numbers");
+
+        }
+
+        if (!Regex.Match(user.Phone, @"^(\+[0-9])$").Success)
+        {
+            throw new Exception("please enter a valid phone number");
+
+        }
+    }
 }
--- LogicServer/WebAPI/Controllers/UserController.cs
+++ LogicServer/WebAPI/Controllers/UserController.cs
@@ -1,6 +1,36 @@
-namespace WebAPI.Controllers;
+using Application.LogicInterfaces;
+using Domain.DTOs;
+using Domain.Model;
+using Microsoft.AspNetCore.Mvc;
 
-public class UserController
+namespace WebAPI.Controllers;
+[ApiController]
+[Route("[controller")]
+
+public class UserController :ControllerBase
 {
+    private readonly IUserLogic userLogic;
+
+    public UserController(IUserLogic userLogic)
+    {
+        this.userLogic = userLogic;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ReturnedUserDTO>> CreateAsync(User user)
+    {
+        try
+        {
+            ReturnedUserDTO returnedUserDto = await userLogic.CreateAsync(user);
+            return Created($"/users/{returnedUserDto.Email}", returnedUserDto);
+            
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
     
 }
