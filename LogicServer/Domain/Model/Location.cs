c101c6eafab86cbc2db66d8921a496cf169af95b
wael85 <dr.wael85@gmail.com>
2022-11-27 15:08:03 +0100

    add trips search page

--- LogicServer/Domain/Model/Location.cs
+++ LogicServer/Domain/Model/Location.cs
@@ -8,5 +8,20 @@ public class Location
     public string StreetName { get; set; }
     public string StreetNumber { get; set; }
     public DateTime ArrivalTime { get; set; }
+
+    public Location(long id, string postCode, string city, string streetName, string streetNumber, DateTime arrivalTime)
+    {
+        Id = id;
+        PostCode = postCode;
+        City = city;
+        StreetName = streetName;
+        StreetNumber = streetNumber;
+        ArrivalTime = arrivalTime;
+    }
+
+    public string getAddress()
+    {
+        return StreetName + " " + StreetNumber + ", " + PostCode + " " + City;
+    }
     
 }
