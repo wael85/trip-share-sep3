f4c7dd3a989d658d41f9d3d4f488c1f51b5b1caf
wael85 <dr.wael85@gmail.com>
2022-11-29 10:53:17 +0100

    add scope

--- PresentationTier/TripShare/Program.cs
+++ PresentationTier/TripShare/Program.cs
@@ -11,6 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new System.Net.Http.HttpClient { BaseAddress = new Uri("https://localhost:7101") });
 builder.Services.AddScoped<IUserClient, UserClientImp>();
 builder.Services.AddScoped<ICarClient, HttpCarClient>();
+builder.Services.AddScoped<ITripClient, HttpTripClient>();
 builder.Services.AddScoped<NotificationService>();
 
 await builder.Build().RunAsync();
