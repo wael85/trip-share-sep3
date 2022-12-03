930fc5fe5eea16f4ee7d5ad3ec190afe044b3145
Juan <315208@via.dk>
2022-12-03 22:59:15 +0100

    create a ticket dataServer

--- DataServer/gRpcServer/src/main/java/via/sep3/grpcserver/services/SeatTicketService.java
+++ DataServer/gRpcServer/src/main/java/via/sep3/grpcserver/services/SeatTicketService.java
@@ -1,48 +1,50 @@
 
 package via.sep3.grpcserver.services;
 
+import io.grpc.Metadata;
+import io.grpc.protobuf.ProtoUtils;
 import io.grpc.stub.StreamObserver;
 import org.lognet.springboot.grpc.GRpcService;
+import org.springframework.beans.factory.annotation.Autowired;
+import via.sep3.grpcserver.Entities.Location;
 import via.sep3.grpcserver.Entities.SeatTicket;
+import via.sep3.grpcserver.Entities.Trip;
 import via.sep3.grpcserver.Entities.User;
+import via.sep3.grpcserver.protobuf.carservices.ErrorResponse;
 import via.sep3.grpcserver.protobuf.ticketservices.*;
 import via.sep3.grpcserver.repositorys.SeatTicketRepository;
+import via.sep3.grpcserver.repositorys.TripRepository;
 import via.sep3.grpcserver.repositorys.UserRepository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
-import java.util.TimeZone;
 
 @GRpcService
 public class SeatTicketService extends TicketServicesGrpc.TicketServicesImplBase {
 
     SeatTicketRepository ticketRepository;
     UserRepository userRepository;
+    TripRepository tripRepository;
 
-    public SeatTicketService(SeatTicketRepository ticketRepository, UserRepository userRepository) {
+    @Autowired
+    public SeatTicketService(SeatTicketRepository ticketRepository, UserRepository userRepository,TripRepository tripRepository) {
         this.ticketRepository = ticketRepository;
         this.userRepository = userRepository;
+        this.tripRepository=tripRepository;
     }
 
     @Override
-    public void getAllUserTicket(TicketsByUserId request, StreamObserver<TicketsByTripResponse> response){
+    public void getAllUserTicket(TicketsByUserId request, StreamObserver<TicketsResponse> response){
         String email =request.getEmail();
         Optional<User> p = userRepository.findByEmail(email);
 
        Optional<List<SeatTicket>> seatTicketList= Optional.ofNullable(ticketRepository.getSeatTicketsByPassenger(p.get()));
-        TicketsByTripResponse.Builder builder =TicketsByTripResponse.newBuilder();
+        TicketsResponse.Builder builder =TicketsResponse.newBuilder();
         for (var t:seatTicketList.get()) {
-            LocationMessage pickUp = LocationMessage.newBuilder().setId(t.getPickUpLocation().getId())
-                            .setPostCode(t.getPickUpLocation().getPostCode()).setCity(t.getPickUpLocation().getCity())
-                            .setStreetName(t.getPickUpLocation().getStreetName()).setStreetNumber(t.getPickUpLocation().getStreetNumber())
-                            .setArrivalTime(t.getPickUpLocation().getArrivalTime()
-                                    .toEpochSecond(TimeZone.getDefault().toZoneId().getRules().getOffset(t.getPickUpLocation().getArrivalTime()))).build();
+            LocationMessage pickUp = buildLocationMessage(t.getPickUpLocation());
 
-            LocationMessage drop = LocationMessage.newBuilder().setId(t.getDropLocation().getId())
-                    .setPostCode(t.getDropLocation().getPostCode()).setCity(t.getDropLocation().getCity())
-                    .setStreetName(t.getDropLocation().getStreetName()).setStreetNumber(t.getDropLocation().getStreetNumber())
-                    .setArrivalTime(t.getDropLocation().getArrivalTime()
-                            .toEpochSecond(TimeZone.getDefault().toZoneId().getRules().getOffset(t.getDropLocation().getArrivalTime()))).build();
+            LocationMessage drop = buildLocationMessage(t.getDropLocation());
 
             builder.addTickets(SeatTicketResponse.newBuilder().setId(t.getId()).setSeatPrice(t.getSeatPrice())
                     .setTotalSeats(t.getTotalSeats()).setPassengerId(t.getPassenger().getEmail()).setTripId(t.getTrip().getId())
@@ -52,6 +54,61 @@ public class SeatTicketService extends TicketServicesGrpc.TicketServicesImplBase
         response.onNext(builder.build());
         response.onCompleted();
     }
+
+    @Transactional
+    @Override
+    public void createTicket(SeatTicketCreationRequest request, StreamObserver<SeatTicketResponse> responseObserver) {
+        Optional<Trip> trip = tripRepository.findById(request.getTripId());
+        if (trip.isEmpty()){
+            Metadata.Key<via.sep3.grpcserver.protobuf.carservices.ErrorResponse> errorResponseKey = ProtoUtils.keyForProto(via.sep3.grpcserver.protobuf.carservices.ErrorResponse.getDefaultInstance());
+            via.sep3.grpcserver.protobuf.carservices.ErrorResponse errorResponse = ErrorResponse.newBuilder()
+                    .setMessage("Trip is not available")
+                    .setStatus(400)
+                    .build();
+            Metadata metadata = new Metadata();
+            metadata.put(errorResponseKey, errorResponse);
+            responseObserver.onError(io.grpc.Status.INVALID_ARGUMENT.withDescription("Trip is not available anymore")
+                    .asRuntimeException(metadata));
+        }else {
+
+            Optional<User> user = userRepository.findById(request.getPassengerId());
+            Location pickUp = new Location();
+            Location dropOff = new Location();
+            for (var t : trip.get().getStops()) {
+                if (t.getId() == request.getPickupId()) {
+                    pickUp = t;
+                }
+                if (t.getId() == request.getDropoffId()) {
+                    dropOff = t;
+                }
+            }
+            SeatTicket seatTicket = new SeatTicket();
+            seatTicket.setTrip(trip.get());
+            seatTicket.setPassenger(user.get());
+            seatTicket.setSeatPrice(request.getSeatPrice());
+            seatTicket.setTotalSeats(request.getTotalSeats());
+            seatTicket.setDropLocation(dropOff);
+            seatTicket.setPickUpLocation(pickUp);
+
+            SeatTicket replay = ticketRepository.save(seatTicket);
+
+            LocationMessage start = buildLocationMessage(replay.getPickUpLocation());
+            LocationMessage end = buildLocationMessage(replay.getDropLocation());
+            SeatTicketResponse ticketResponse = SeatTicketResponse.newBuilder().setId(replay.getId()).setSeatPrice(replay.getSeatPrice())
+                    .setPassengerId(replay.getPassenger().getEmail()).setTripId(replay.getTrip().getId()).setPickup(start).setDropoff(end).build();
+
+            responseObserver.onNext(ticketResponse);
+            responseObserver.onCompleted();
+        }
+    }
+
+    public synchronized LocationMessage buildLocationMessage(Location location){
+        return LocationMessage.newBuilder().setId(location.getId())
+                .setPostCode(location.getPostCode()).setCity(location.getCity())
+                .setStreetName(location.getStreetName()).setStreetNumber(location.getStreetNumber())
+                .setArrivalTime(location.getArrivalTime().toString()).build();
+    }
+
 
 }
 
