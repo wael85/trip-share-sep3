0ac591baee2877130094c74ad41eb17b1d673908
Juan <315208@via.dk>
2022-12-07 21:53:51 +0100

    Login presentationTier

---
+++ PresentationTier/HttpClient/ClientInterfaces/IAuthService.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Claims;
+
+namespace HttpClients.ClientInterface;
+
+public interface IAuthService
+{
+    Task LoginAsync(string username, string password);
+    Task LogoutAsync();
+
+    Task<ClaimsPrincipal> GetAuthAsync();
+
+    Action<ClaimsPrincipal> OnAuthStateChanged { get; set; }
+}
